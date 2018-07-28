707ffc6fa60f7fd64372b54d9aa1f7d71229068e
rodrigozuniga <rodrigozuniga@gmail.com>
2018-07-27 21:17:45 -0700

    first commit

---
+++ first_test.py
@@ -0,0 +1,10 @@
+import numpy as np, pandas as pd
+print('\n',100)
+np.arange(0,10)
+for i in [np.arange(0,10)]:
+    print(i)
+
+import matplotlib.pyplot as pl
+pl.hist(np.arange(0,100))
+pl.hist(np.random.randn(10000))
+print('ssss')
